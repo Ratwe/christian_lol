2c872620a98ecc5de2b1a1566a5eb0b42ecfcea0
Artem Dyachenko <artemon1324@yandex.ru>
2023-08-18 15:07:02 +0300

    Almost done pytest setup and first tests

--- tests/test_auth.py
+++ tests/test_auth.py
@@ -2,13 +2,13 @@ import pytest
 from sqlalchemy import insert, select
 
 from src.auth.models import role
-from tests.conftest import client, async_session_maker
+from tests.conftest import async_session_maker, client
 
 
-
+@pytest.mark.asyncio
 async def test_add_role():
     async with async_session_maker() as session:
-        stmt = insert(role).values(id=1, name="pytest_admin", permissions=None)
+        stmt = insert(role).values(id=1, name="admin", permissions=None)
         await session.execute(stmt)
         await session.commit()
 
@@ -17,13 +17,15 @@ async def test_add_role():
         print(result.all())
 
 
-# def test_register():
-#     client.post("/auth/register", json={
-#         "email": "pytest_email",
-#         "password": "pytest_password",
-#         "is_active": True,
-#         "is_superuser": False,
-#         "is_verified": False,
-#         "username": "pytest_username",
-#         "role_id": 0
-#     })
+def test_register():
+    response = client.post("/auth/register", json={
+        "email": "pytest_email",
+        "password": "pytest_password",
+        "is_active": True,
+        "is_superuser": False,
+        "is_verified": False,
+        "username": "pytest_username",
+        "role_id": 0
+    })
+
+    assert response.status_code == 201
